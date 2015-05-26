5be6586f9580fe082ad25ce9f6a2bd426943d4f1
Rob Howard <rob@robhoward.id.au>
2015-05-27 01:34:35 +1000

    Some initial noodlings with Board, Player PositionState, move, playerAt and phantom types.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,35 @@
+{-# LANGUAGE GADTs #-}
+
 module Main where
 
 main :: IO ()
 main = putStrLn "Hi"
+
+data PositionState = Occupied Player | NotTaken
+  deriving(Show)
+
+data Player = PlayerX | PlayerO
+  deriving(Show) -- TODO: Custom Show
+
+data Position = TL | TC | TR
+              | ML | MC | MR
+              | BL | BC | BR
+  deriving(Show) -- TODO: Custom Show
+
+data Finished
+data Unfinished
+data Board s = Board [(Position, Player)]
+  deriving(Show) -- TODO: Custom Show
+
+
+
+data MoveResult b where
+  PositionOccupied   :: MoveResult b
+  ActiveGame         :: MoveResult (Board Unfinished)
+  FinishedGame       :: MoveResult (Board Finished)
+
+-- move :: (Board Unfinished) -> MoveResult
+
+playerAt :: (Board a) -> Position -> PositionState
+playerAt (Board ps) TL = Occupied PlayerX
+playerAt (Board ps) p  = NotTaken
